934f72d8df0e30d242fd283120c88dae236d310b
Cody Hui <Aquaficial@Codys-MacBook-Pro.local>
2016-11-13 20:30:32 -0800

    Added scrolling on resume page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function($){
 		//this applies only if secondary nav is below intro section
 		belowNavHeroContent = $('.sub-nav-hero'),
 		headerHeight = mainHeader.height();
+		currentSection = $(".cd-secondary-nav .active");
 	
 	//set scrolling variables
 	var scrolling = false,
@@ -12,7 +13,7 @@ jQuery(document).ready(function($){
 		scrollDelta = 10,
 		scrollOffset = 150,
 		isHidden = false;
-
+		
 	mainHeader.on('click', '.nav-trigger', function(event){
 		// open primary navigation on mobile
 		event.preventDefault();
@@ -23,22 +24,34 @@ jQuery(document).ready(function($){
 		$(this).scrollTop(0);
 		$(".headers").on("click", function( e ) {
 			e.preventDefault();
-			var distance = $(this).offset().top - $( $(this).attr('href') ).offset().top;
-			console.log($(this).offset().top - $( $(this).attr('href') ).offset().top);
-			console.log(isHidden);
+			var distance = Math.abs($(this).offset().top - $( $(this).attr('href') ).offset().top) - 70;
 			offsetDistance = 0; 
-			if((Math.abs(distance) > 350 && !isHidden) || (Math.abs(distance) < 350 & isHidden)) {
+			if((Math.abs(distance) > 350 && !isHidden) || (Math.abs(distance) < 300 & isHidden)) {
 				offsetDistance = 100;
 			} else {
 				offsetDistance = 150;
 			}
-			$("body, html").animate({ 
+			$("body, html").stop(true, true).animate({ 
 				scrollTop: $( $(this).attr('href') ).offset().top - offsetDistance
 			}, 1000);
+			currentSection = $(this);
 		});
 	});
 
 	$(window).on('scroll', function(){
+		var position = $(this).scrollTop() + 170;
+
+		$('.section').each(function() {
+	        var target = $(this).offset().top;
+	        var id = $(this).attr('id');
+
+	        if (position >= target) {
+	        	currentSection = $('.cd-secondary-nav > ul > li > a[href="#' + id + '"]');
+	            $('.cd-secondary-nav > ul > li > a').removeClass('active');
+            	$('.cd-secondary-nav > ul > li > a[href="#' + id + '"]').addClass('active');
+	        }
+    	});
+
 		if( !scrolling ) {
 			scrolling = true;
 			(!window.requestAnimationFrame)
@@ -81,13 +94,13 @@ jQuery(document).ready(function($){
 		
 		if (previousTop >= currentTop ) {
 	    	//if scrolling up... 
-	    	if( currentTop < secondaryNavOffsetTop && !changingLink) {
+	    	if( currentTop < secondaryNavOffsetTop) {
 	    		//secondary nav is not fixed
 	    		mainHeader.removeClass('is-hidden');
 	    		secondaryNavigation.removeClass('fixed slide-up');
 	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
 	    		isHidden = false;
-	    	} else if( previousTop - currentTop > scrollDelta && !changingLink) {
+	    	} else if( previousTop - currentTop > scrollDelta) {
 	    		//secondary nav is fixed
 	    		mainHeader.removeClass('is-hidden');
 	    		secondaryNavigation.removeClass('slide-up').addClass('fixed'); 
@@ -97,13 +110,13 @@ jQuery(document).ready(function($){
 	    	
 	    } else {
 	    	//if scrolling down...	
-	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset && !changingLink) {
+	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset) {
 	 	  		//hide primary nav
 	    		mainHeader.addClass('is-hidden');
 	    		secondaryNavigation.addClass('fixed slide-up');
 	    		belowNavHeroContent.addClass('secondary-nav-fixed');
 	    		isHidden = true;
-	    	} else if( currentTop > secondaryNavOffsetTop && !changingLink) {
+	    	} else if( currentTop > secondaryNavOffsetTop) {
 	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset 
 	    		mainHeader.removeClass('is-hidden');
 	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
